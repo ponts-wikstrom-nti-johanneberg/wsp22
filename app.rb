24a9fc2bb704e618bed6e5cfd1fc166e8e0d2899
ponts-wikstrom-nti-johanneberg <74712747+ponts-wikstrom-nti-johanneberg@users.noreply.github.com>
2022-04-11 10:31:44 +0200

    Fixat med login och register, ska göra så att man måste vara inloggad för att göra nya ratings.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'slim'
 require 'sqlite3'
+require 'bcrypt'
 require 'sinatra/reloader' if development?
 
 enable :sessions
@@ -9,9 +10,49 @@ get ('/') do
     slim(:start)
 end
 
+get ('/register') do
+    slim(:register)
+end
+
+get ('/login') do
+    slim(:login)
+end
+
 get('/show_bronze') do
     slim(:show_bronze)
 end
+
+post('/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/databas_be_like.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM user WHERE Name = ?",username).first
+    pwdigest = result["Pwdigest"]
+    id = result["id"]
+    if BCrypt::Password.new(pwdigest) == password
+      session[:id] = id
+      redirect('/')
+    else
+      "Wrong Password"
+    end
+  
+  end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+  
+    if password == password_confirm
+      password_digest = BCrypt::Password.create(password)
+      db = SQLite3::Database.new('db/databas_be_like.db')
+      db.execute("INSERT INTO user (Name,Pwdigest) VALUES (?,?)",username,password_digest)
+      redirect('/')
+    else
+      "The passwords were not meant to be."
+    end
+  end
 
 get('/make_bronze') do
     db = SQLite3::Database.new("db/databas_be_like.db")
@@ -22,9 +63,6 @@ get('/make_bronze') do
 end
 
 post('/make_bronze') do
-    p "hej"
-    p params
-    p "hej"
     db = SQLite3::Database.new("db/databas_be_like.db")
     db.results_as_hash = true
     params.each do |key, value|
