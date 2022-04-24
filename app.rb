3dab7d305aeb7e68f28f8b576ec7e8034f1ae9f7
ponts-wikstrom-nti-johanneberg <74712747+ponts-wikstrom-nti-johanneberg@users.noreply.github.com>
2022-04-24 23:42:57 +0200

    Klar med allting, fick inte yardoc på model.rb att fungera

--- app.rb
+++ app.rb
@@ -3,14 +3,7 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 require 'sinatra/reloader' if development?
-require_relative '/model/model.rb'
-
-# Vad jag ska göra:
-# Lägga till Edit button, eventuellt att jag gör det på varje rad/varje kort. Tar isåfall all information från enskilda raden/kortet och går in på en ny sida som isåfall ändrar value på det kortet.
-# Hjälpfunktioner
-# Delete button
-# User settings, ska kunna editta ens egna, kunna eventuellt se ens egna
-# Admin settings? Kunna edita/ta bort andras?
+require_relative 'model/model.rb'
 
 enable :sessions
 include Model
@@ -22,30 +15,59 @@ get ('/') do
     slim(:start)
 end
 
-# 
+# Display user registration.
+#
 get ('/users/register') do
     slim(:"/users/register")
 end
 
+# Display user login
+#
 get ('/users/login') do
     slim(:"/users/login")
 end
 
+# Display all cards in a rated list, with the cards that has the lowest rating furthers down, and the cards with the highest ratings at the top.
+#
+# @rating_1star [Hash], hash with cards that has 1 star 
+# @rating_2star [Hash], hash with cards that has 2 star 
+# @rating_3star [Hash], hash with cards that has 3 star 
+# @rating_4star [Hash], hash with cards that has 4 star 
+# @rating_5star [Hash], hash with cards that has 5 star 
+# @username_list [hash], hash with a list of usernames that has made a list
+#
+# @see Model#show_bronze
 get('/show_bronze') do
     rating_1star, rating_2star, rating_3star, rating_4star, rating_5star, username_list = {}, {}, {}, {}, {}, {}
     show_bronze(rating_1star, rating_2star, rating_3star, rating_4star, rating_5star, username_list)
     slim(:show_bronze, locals:{rating_1star:rating_1star, rating_2star:rating_2star, rating_3star:rating_3star, rating_4star:rating_4star, rating_5star:rating_5star, ratinglist:ratinglist, username_list:username_list})
 end
 
+# Display all cards in a rated list, with the cards that has the lowest rating furthers down, and the cards with the highest ratings at the top. All of the lists shown can only the user who is logged in see.
+#
+# @rating_1star [Hash], hash with cards that has 1 star 
+# @rating_2star [Hash], hash with cards that has 2 star 
+# @rating_3star [Hash], hash with cards that has 3 star 
+# @rating_4star [Hash], hash with cards that has 4 star 
+# @rating_5star [Hash], hash with cards that has 5 star 
+# @username_list [hash], hash with a list of usernames that has made a list
+#
+# @see Model#your_lists
 get('/users/your_lists') do
     rating_1star, rating_2star, rating_3star, rating_4star, rating_5star, username_list = {}, {}, {}, {}, {}, {}
     your_lists(rating_1star, rating_2star, rating_3star, rating_4star, rating_5star, username_list)
     slim(:"/users/your_lists", locals:{rating_1star:rating_1star, rating_2star:rating_2star, rating_3star:rating_3star, rating_4star:rating_4star, rating_5star:rating_5star, ratinglist:ratinglist, username_list:username_list})
 end
 
-# Funktionen kollar ifall personen som loggar in använder sig av rätt lösenord med motsvarande användarnamn.
+# Attempts login and updates the session
+#
+# @param [String] username, The username
+# @session [String] logging, Checks the time between first attempt of login and second attempt
+# Time.now [String] logging, Second attempt of logging
+# 
+# redirects to '/error' if the time between first and second attemt is less than 10 seconds
 before ('/users/login') do
-    username = params[:username]
+    # username = params[:username]
     if session[:logging] != nil
         if Time.now - session[:logging] < 10
             redirect('/error/You_are_logging_in_too_fast._Please_slow_down.')
@@ -53,18 +75,14 @@ before ('/users/login') do
     end
 end
 
-post('/users/login') do
-    username = params[:username]
-    password = params[:password]
-    session[:logging] = Time.now
-    username = bcrypt(username, password)
-    if username == nil
-        redirect('/error/Wrong_password_or_username.')
-    end
-    session[:username] = username
-    redirect('/')
-end
+# Attempts login and updates the session
+# @param [String], username, The username
+# @param [String], password, The password
+# @param [String], password_confirm, The repeated password
 
+# if username == nil, redirect to '/error'
+# else, redirect to '/'
+# @see Model#new_user
 post('/users/new') do
     username = params[:username]
     password = params[:password]
@@ -78,6 +96,35 @@ post('/users/new') do
     end
 end
 
+#  Attempts login and updates the session
+#
+# @param [String] username, The username
+# @param [String] password, The password
+# @session [String] username, The username
+# @session [String] logging, Checks the time between first attempt of login and second attempt
+#
+# if username == nil, redirects to '/error'
+# else, redirects to '/'
+#
+# @see Model#bcrypt
+post('/users/login') do
+    username = params[:username]
+    password = params[:password]
+    session[:logging] = Time.now
+    username = bcrypt(username, password)
+    if username == nil
+        redirect('/error/Wrong_password_or_username.')
+    end
+    session[:username] = username
+    redirect('/')
+end
+
+# Display all cards for the user to rate
+#
+# if username == nil, redirects to '/login'
+# else redirects to '/make_yours_bronze'
+#
+# @see Model#select_cards
 get('/make_bronze') do
     if username == nil # Tidigare satt username som nil under enable :sessons, på get ('/login') får vi att username är något anant och går därför till den tänkta sidan.
         slim(:login)
@@ -87,6 +134,19 @@ get('/make_bronze') do
     end
 end
 
+# Creates new place for user to see cards to rate
+#
+# @db [Hash], connects database with ruby
+# @param [Integer], card_id, The id for the specific card
+# @param [Integer], card_rating, The value of the rating of one specific card
+# @ratinglist [Integer], checks which list the cards will join
+# @user_id [Integer], checks the id of the user who is logged on the site at that moment
+#
+# params.each, a loop that checks each specific card and its values
+# redirects to '/show_bronze'
+#
+# @see Model#get_user
+# @see Model@get_ratinglist
 post('/make_bronze') do
     db = connect_to_db('db/databas_be_like.db')
     username = session[:username]
@@ -107,6 +167,31 @@ post('/make_bronze') do
     redirect('/show_bronze')
 end
 
+# Displays all cards in a specific rating list that the user wants to edit
+#
+# @param [Integer], ratinglist, Checks which rating list that gets edited
+#
+# redirects to '/edit'
+#
+# @see Model#edit_list
+get('/:index/edit') do
+    ratinglist = params[:index]
+    result = edit_list(ratinglist)
+    slim(:edit, locals:{result:result})
+end
+
+# Updates an already existing rating list
+#
+# @param [Integer], ratinglist, Checks which rating list that gets edited
+# @db [Hash], connects database with ruby
+# @param [Integer], card_id, The id for the specific card
+# @param [Integer], card_rating, The value of the rating of one specific card
+#
+# params.each, a loop that checks each specific card and its values
+# redirects to '/show_bronze'
+#
+# @see Model#get_user
+# @see Model@get_ratinglist
 post ('/:index/update') do
     ratinglist = params[:index]
     db = connect_to_db('db/databas_be_like.db')
@@ -123,18 +208,21 @@ post ('/:index/update') do
     redirect('/show_bronze')
 end
 
+# Deletes an existing rating list
+#
+# @param [Integer], ratinglist, Checks which rating list that gets deleted
+#
+# redirects to '/show_bronze'
+#
+# @see Model#delete_list
 post ('/:index/delete') do
     ratinglist = params[:index]
     delete_list(ratinglist)
     redirect('/show_bronze')
 end
 
-get('/:index/edit') do
-    ratinglist = params[:index]
-    result = edit_list(ratinglist)
-    slim(:edit, locals:{result:result})
-end
-
+# Displays an error message
+#
 get('/error/:error_message') do
     error_message = params[:error_message].split("_").join(" ")
     slim(:"/error/error", locals:{username:session[:username], error_message:error_message})
--- model/model.rb
+++ model/model.rb
@@ -4,63 +4,90 @@ require 'sqlite3'
 require 'bcrypt'
 
 module Model
-
+    # Connects database with ruby
+    # @return [Hash] containing database
     def connect_to_db(path)
         db = SQLite3::Database.new(path)
         db.results_as_hash = true
         return db
     end
-
+    # Makes a rating list with cards that has 1 star, 2 star, 3 star, 4 star and 5 star, aswell as with a username
+    # 
+    # @db [Hash], connects database with ruby
+    # @ratinglist [Integer], selects the last made ratinglist in the table user_ratinglist_relation
+    # @rating_1star [Hash], hash with cards that has 1 star 
+    # @rating_2star [Hash], hash with cards that has 2 star 
+    # @rating_3star [Hash], hash with cards that has 3 star 
+    # @rating_4star [Hash], hash with cards that has 4 star 
+    # @rating_5star [Hash], hash with cards that has 5 star 
+    # @username_list [hash], hash with a list of usernames that has made a list
+    # @return [Hash], containing the data of all cards into a rating list
     def show_bronze(rating_1star, rating_2star, rating_3star, rating_4star, rating_5star, username_list)
         db = connect_to_db('db/databas_be_like.db')
         ratinglist = db.execute("SELECT ratinglist FROM user_ratinglist_relation").last["ratinglist"].to_i
-        # p "ratinglist är #{ratinglist}"
-        # p ratinglist
         (1..ratinglist).to_a.each do |index|
-            # ratinglist = ratinglist.to_i
             rating_1star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='1' AND ratinglist=?)", index)
-            # rating_1star[ratinglist] = rating_1star
-            # return rating_0star.to_s
-            # p "rating_1star är #{rating_1star}"
             rating_2star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='2' AND ratinglist=?)", index)
-            # rating_1star[ratinglist] = rating_2star
             rating_3star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='3' AND ratinglist=?)", index)
-            # rating_1star[ratinglist] = rating_3star
             rating_4star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='4' AND ratinglist=?)", index)
-            # rating_1star[ratinglist] = rating_4star
             rating_5star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='5' AND ratinglist=?)", index)
-            # rating_1star[ratinglist] = rating_5star
             username_list[index] = db.execute("SELECT Name FROM user WHERE id IN (SELECT user_id FROM user_ratinglist_relation WHERE ratinglist=?)", index).first
-            p username_list
         end
     end
 
+    # Makes all of the rating list that the user who is logged in has with cards that has 1 star, 2 star, 3 star, 4 star and 5 star, aswell as with a username
+    # 
+    # @db [Hash], connects database with ruby
+    # @id [Integer], selects the id of the user who is logged on at the moment
+    # @ratinglist [Integer], selects the last made ratinglist in the table user_ratinglist_relation
+    # @rating_1star [Hash], hash with cards that has 1 star 
+    # @rating_2star [Hash], hash with cards that has 2 star 
+    # @rating_3star [Hash], hash with cards that has 3 star 
+    # @rating_4star [Hash], hash with cards that has 4 star 
+    # @rating_5star [Hash], hash with cards that has 5 star 
+    # @username_list [hash], hash with a list of rating lists that the user has
+    # @return [Hash], containing the new data of all cards into a rating list
     def your_lists(rating_1star, rating_2star, rating_3star, rating_4star, rating_5star, username_list)
         db = connect_to_db('db/databas_be_like.db')
         id = db.execute("SELECT id FROM user WHERE Name=?", session[:username]).last["id"].to_i
-        p id
         ratinglist = db.execute("SELECT ratinglist FROM user_ratinglist_relation").last["ratinglist"].to_i
-        # p "ratinglist är #{ratinglist}"
-        # p ratinglist
         (1..ratinglist).to_a.each do |index|
-            # ratinglist = ratinglist.to_i
             rating_1star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='1' AND ratinglist=? AND user_id=?)", index, id)
-            # rating_1star[ratinglist] = rating_1star
-            # return rating_0star.to_s
-            # p "rating_1star är #{rating_1star}"
             rating_2star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='2' AND ratinglist=? AND user_id=?)", index, id)
-            # rating_1star[ratinglist] = rating_2star
             rating_3star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='3' AND ratinglist=? AND user_id=?)", index, id)
-            # rating_1star[ratinglist] = rating_3star
             rating_4star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='4' AND ratinglist=? AND user_id=?)", index, id)
-            # rating_1star[ratinglist] = rating_4star
             rating_5star[index] = db.execute("SELECT * FROM cards WHERE id IN (SELECT cards_id FROM user_ratinglist_relation WHERE show_rating='5' AND ratinglist=? AND user_id=?)", index, id)
-            # rating_1star[ratinglist] = rating_5star
             username_list[index] = db.execute("SELECT Name FROM user WHERE id IN (SELECT user_id FROM user_ratinglist_relation WHERE ratinglist=?)", index).first
-            p username_list
         end
     end
 
+    # Checks if the firstly written password is the same as the second written password
+    #
+    # @db [Hash], Connects database with ruby
+    #
+    # @return [Hash], if passwords matches, The username and the digested password of the new user
+    # @return [Nil], if passwords does not match
+    def new_user(username, password, password_confirm)
+        if password == password_confirm
+            password_digest = BCrypt::Password.create(password)
+            db = SQLite3::Database.new('db/databas_be_like.db')
+            return db.execute("INSERT INTO user (Name,Pwdigest) VALUES (?,?)",username,password_digest)
+        else
+            return nil
+        end
+    end
+
+    # Checks if the digested password is the same as the password the user uses
+    #
+    # @db [Hash], connects database with ruby
+    # @result [Hash], selects all of the tables inside of the user table where Name is username
+    # @pwdigest [String], picks up the table "Pwdigest" from user
+    # @id [Integer], picks up the id from the table user
+    # @option params [String], username, The username
+    # @option params [String], password, The password
+    #
+    # @return [String], if password matches pwdigest
+    # @return [Nil], if password does not match pwdigest 
     def bcrypt(username, password)
         db = connect_to_db('db/databas_be_like.db')
         result = db.execute("SELECT * FROM user WHERE Name = ?",username).first
@@ -73,27 +100,33 @@ module Model
         end
     end
 
-    def new_user(username, password, password_confirm)
-        if password == password_confirm
-            password_digest = BCrypt::Password.create(password)
-            db = SQLite3::Database.new('db/databas_be_like.db')
-            return db.execute("INSERT INTO user (Name,Pwdigest) VALUES (?,?)",username,password_digest)
-        else
-            return nil
-        end
-    end
-
+    # Selects all of the fields in the table cards
+    #
+    # @db [Hash], connects database with ruby
+    #
+    # @return [Hash], All of the fields from the table cards
     def select_cards()
         db = connect_to_db('db/databas_be_like.db')
         return result = db.execute("SELECT * FROM cards")
     end
 
+    # Selects the id from user where the username is the logged in user
+    #
+    # @session [String], username, The username of the logged in user
+    # @user_id [Integer], Selected id from the table user
+    #
+    # @return [Integer], id from the table user
     def get_user_id(db, username)
         user_id = db.execute("SELECT id FROM user WHERE Name = ?", username).first
         user_id = user_id[0].to_i
         return user_id
     end
 
+    #Selects the last ratinglist from user_ratinglist_relation
+    #
+    # @ratinglist [Integer], Selected ratinglist from the table user_ratinglist_relation
+    #
+    # @return [Integer], ratinglist, either it returns the first rating list, or any other after with an integer with a value that incrementate 1
     def get_ratinglist(db)
         ratinglist = db.execute("SELECT ratinglist FROM user_ratinglist_relation").last
         p "Ratinglist är #{ratinglist}"
@@ -107,23 +140,8 @@ module Model
         return ratinglist
     end
 
+    # Vi kan gärna kolla på denna funktion sedan. Vet inte hur jag ska lägga in den här inne...
     # def make_bronze(key, value, ratinglist, user_id)
-    #     db = SQLite3::Database.new("db/databas_be_like.db")
-    #     db.results_as_hash = true
-    #     user_id = db.execute("SELECT id FROM user WHERE Name = ?", username).first
-    #     user_id = user_id[0].to_i
-    #     p "user_id är #{user_id}"
-    #     ratinglist = db.execute("SELECT ratinglist FROM user_ratinglist_relation").last
-    #     p "Ratinglist är #{ratinglist}"
-    #     if ratinglist == nil
-    #         ratinglist = 0
-    #     else 
-    #         ratinglist = ratinglist[ratinglist.length - 1].to_i
-    #         p "Ratinglist är därmed #{ratinglist}"
-    #     end
-    #     ratinglist += 1
-    #     session[:ratinglist] = ratinglist
-    #     p "Ny ratinglist är #{ratinglist}"
     #     params.each do |key, value|
     #         p key
     #         p value
@@ -135,11 +153,15 @@ module Model
     #     end
     # end
 
+    # Deletes the rating list that the user owns
+    #
+    # db [Hash], connects database with ruby
+    #
+    # @return, deleted rating list
     def delete_list(ratinglist)
         db = SQLite3::Database.new("db/databas_be_like.db")
         db.execute("DELETE FROM user_ratinglist_relation WHERE ratinglist = ?",ratinglist)
     end
-
 
     # Vi kan gärna kolla på denna funktion sedan. Vet inte hur jag ska lägga in den här inne...
     # def update_list(ratinglist, key, value)
@@ -148,6 +170,11 @@ module Model
     #     return show_rating
     # end
 
+    # Updates one rating list that the user owns
+    #
+    # db [Hash], connects database with ruby
+    #
+    # return [Hash], All of the fields in the table cards that has the same id as the cards_id in the table user_ratinglist_relation where the ratinglist equals the ratinglist that the user clicked on
     def edit_list(ratinglist)
         db = connect_to_db('db/databas_be_like.db')
         return result = db.execute("SELECT * FROM cards INNER JOIN user_ratinglist_relation ON cards.id = cards_id WHERE ratinglist=?", ratinglist)
